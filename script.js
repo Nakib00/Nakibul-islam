aa7007b9e675da455ed447ecc3e031e3a76e3082
Nakib00 <2022142@iub.edu.bd>
2023-09-30 13:48:32 +0600

    updatae

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
+// Protfolio button show 
 function showContent(category) {
   alert("Showing content for: " + category);
 }
-
 
 // Change container in protfolio container
 function showContent(type) {
@@ -46,7 +46,7 @@ function showContent(type) {
       </div>
       <div class="explor-button">
         <div class="see-more">
-          <button class="card__btn">Seemore <span>&rarr;</span></button>
+          <a href="https://www.youtube.com/@mdnakibulislam8183"><button class="card__btn">Seemore<span>&rarr;</span></button></a>
         </div>
       </div>
     </div> 
@@ -59,30 +59,30 @@ function showContent(type) {
         <div class="grid1">
           <div class="grid__item">
             <div class="card1">
-              <img class="card__img" src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80" alt="Snowy Mountains">
+              <iframe class="card__img" src="https://www.youtube.com/embed/aUaX_P7Il_U" frameborder="0" allowfullscreen></iframe>
               <div class="card__content">
-                <h1 class="card__header">A starry night</h1>
-                <p class="card__text">Look up at the night sky, and find yourself <strong>immersed</strong>dddeeee fddadf .</p>
+                <h1 class="card__header">Avash | Camera</h1>
+                <p class="card__text">I edited a visual video for Avash, a renowned music brand in Bangladesh.</p>
               </div>
             </div>
           </div>
       
           <div class="grid__item">
             <div class="card1">
-              <img class="card__img" src="https://images.unsplash.com/photo-1485160497022-3e09382fb310?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80" alt="Desert">
+            <iframe class="card__img" src="https://youtu.be/ZZpp4WMokZ0?si=QxbVZ3fOFPicgLV1" frameborder="0" allowfullscreen></iframe>
               <div class="card__content">
-                <h1 class="card__header">Misty mornings</h1>
-                <p class="card__text">Capture the stunning <strong>essence</strong> of the early morning sunrise in the Californian wilderness.</p>
+                <h1 class="card__header">FENI GOVERNMENT COLLEGE Documentary</h1>
+                <p class="card__text">I created documentary videos for Feni Government College.</p>
               </div>
             </div>
           </div>
       
           <div class="grid__item">
             <div class="card1">
-              <img class="card__img" src="https://images.unsplash.com/photo-1506318164473-2dfd3ede3623?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3300&q=80" alt="Canyons">
+            <iframe class="card__img" src="https://youtu.be/GjnC-ZXZ9rQ?si=qa5gw2a5CWN_pVOy" frameborder="0" allowfullscreen></iframe>
               <div class="card__content">
-                <h1 class="card__header">Utah sunsets</h1>
-                <p class="card__text">Sunsets over the <strong>stunning</strong> Utah Canyonlands, is truly something much more than incredible.</p>
+                <h1 class="card__header">Shoes commercial</h1>
+                <p class="card__text">I directed the production of this commercial video.</p>
               </div>
             </div>
           </div>
@@ -90,7 +90,7 @@ function showContent(type) {
       </div>
       <div class="explor-button">
         <div class="see-more">
-          <button class="card__btn">Seemore <span>&rarr;</span></button>
+          <a href="https://www.youtube.com/@mdnakibulislam8183"><button class="card__btn">Seemore<span>&rarr;</span></button></a>
         </div>
       </div>
     </div> 
@@ -103,7 +103,7 @@ function showContent(type) {
         <div class="grid1">
           <div class="grid__item">
             <div class="card1">
-              <img class="card__img" src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80" alt="Snowy Mountains">
+            <iframe class="card__img" src="https://www.youtube.com/embed/aUaX_P7Il_U" frameborder="0" allowfullscreen></iframe>
               <div class="card__content">
                 <h1 class="card__header">A starry night</h1>
                 <p class="card__text">Look up at the night sky, and find yourself <strong>immersed</strong>dddeeee fddadf .</p>
@@ -113,7 +113,7 @@ function showContent(type) {
       
           <div class="grid__item">
             <div class="card1">
-              <img class="card__img" src="https://images.unsplash.com/photo-1485160497022-3e09382fb310?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80" alt="Desert">
+            <iframe class="card__img" src="https://www.youtube.com/embed/aUaX_P7Il_U" frameborder="0" allowfullscreen></iframe>
               <div class="card__content">
                 <h1 class="card__header">Misty mornings</h1>
                 <p class="card__text">Capture the stunning <strong>essence</strong> of the early morning sunrise in the Californian wilderness.</p>
@@ -123,7 +123,7 @@ function showContent(type) {
       
           <div class="grid__item">
             <div class="card1">
-              <img class="card__img" src="https://images.unsplash.com/photo-1506318164473-2dfd3ede3623?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3300&q=80" alt="Canyons">
+            <iframe class="card__img" src="https://www.youtube.com/embed/aUaX_P7Il_U" frameborder="0" allowfullscreen></iframe>
               <div class="card__content">
                 <h1 class="card__header">Utah sunsets</h1>
                 <p class="card__text">Sunsets over the <strong>stunning</strong> Utah Canyonlands, is truly something much more than incredible.</p>
@@ -134,7 +134,7 @@ function showContent(type) {
       </div>
       <div class="explor-button">
         <div class="see-more">
-          <button class="card__btn">Seemore <span>&rarr;</span></button>
+          <a href="https://www.youtube.com/@mdnakibulislam8183"><button class="card__btn">Seemore<span>&rarr;</span></button></a>
         </div>
       </div>
     </div> 
